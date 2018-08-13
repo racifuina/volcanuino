b1959d92adc1d9983e1790577d5a928586a97c97
Rigoberto Acifuina <racifuina@Rigobertos-MacBook-Pro-3.local>
2018-08-13 12:00:02 -0600

    inicio de repositorio

---
+++ emisor/emisor.ino
@@ -0,0 +1,48 @@
+#include <VirtualWire.h> //Libreria encargada de la comunicación RF.
+
+char *controller; //Variable para guardar el comando que se enviará
+int cable1 = A1; //Pin analogico Anillo de Seguridad 1
+int cable2 = A2; //Pin analogico Anillo de Seguridad 2
+
+void setup() {
+    pinMode(13, OUTPUT); //Modo salida para el pin 13 (LED DEL ARDUINO)
+    vw_set_ptt_inverted(true); //Requerido por el modulo
+    vw_set_tx_pin(12); //Pin de transmision (TX)
+    vw_setup(600); //Velocidad de Transmisión
+}
+
+void loop() {
+    if (analogRead(cable1) != 1023) { //si la lectura del anillo de seguridad 1 no es igual a 1023 (5 volts) se envía una alerta
+        sendDanger();
+    } else if (analogRead(cable2) != 1023) { //si la lectura del anillo de seguridad 2 no es igual a 1023 (5 volts) se envía una alerta
+        sendDanger();
+    } else { //si ambos anillos se encuentran bien, enviamos comando 0
+        sendOK();
+    }
+    delay(2000); //Espera de 2 segundos entre cada lectura.
+}
+
+
+/*
+void sendOK();
+Función encargada de enviar el comando 0 que indica que las lecturas
+en ambos anillos de seguridad esta fuera de peligro. 
+*/
+void sendOK(){ 
+    digitalWrite(13, 0);  //Apagar LED del arduino
+    controller="0"; //set comando 0
+    vw_send((uint8_t *)controller, strlen(controller));  //envio del comando
+    vw_wait_tx(); //Esperar a que el mensaje se envie completamente
+}
+
+/*
+void sendDanger();
+Función encargada de enviar el comando 1 que indica que la lectura
+de algún anillo de seguridad muestra que hay lava. 
+*/
+void sendDanger() {
+    digitalWrite(13, 1); //encender LED del arduino
+    controller = "1"; //set comando 1
+    vw_send((uint8_t *)controller, strlen(controller));//envio del comando 
+    vw_wait_tx(); //Esperar a que el mensaje se envie completamente
+}
---
+++ receptor/receptor.ino
@@ -0,0 +1,86 @@
+#include <VirtualWire.h> //Libreria encargada de la comunicación RF.
+
+void setup() {
+    pinMode(13, OUTPUT); //Modo salida para el pin 13 (LED DEL ARDUINO)
+    pinMode(3, OUTPUT); //Modo salida para el Pin de LED'S
+    pinMode(6, OUTPUT); //Modo salida para el Pin del BUZZER
+    vw_set_ptt_inverted(true); //Requerido por el modulo
+    vw_set_rx_pin(12); //Pin de recepción (RX)
+    vw_setup(600); //Velocidad de Transmisión
+    vw_rx_start(); //Empezar la recepción de datos
+    
+}
+
+void loop() {
+    
+    //VW_MAX_MESSAGE_LEN valor constante de longitud de mensajes de la libreria VirtualWire
+    
+    uint8_t buf[VW_MAX_MESSAGE_LEN]; //Buffer
+    uint8_t buflen = VW_MAX_MESSAGE_LEN; //Longitud del Buffer
+    
+    if (vw_get_message(buf, &buflen)) { //Recepción del Buffer de comunicación
+        if(buf[0]=='1'){ //si el buffer es igual a '1' se alerta a los pobladores y se activa el protocolo de seguridad
+            alertPeople();
+        } else { //de lo contrario se apagan las alarmas.
+          allGood();
+        }
+    } 
+}
+
+/*
+void allGood();
+Función que indica que las lecturas
+en ambos anillos de seguridad esta fuera de peligro. 
+
+Apaga todos los sistemas de alarma
+*/
+
+void allGood() {
+    digitalWrite(13, 0);
+    digitalWrite(3, 0);
+    digitalWrite(6, 0);
+}
+
+/*
+void alertPeople();
+Función que indica que la lectura de algún anillo de seguridad muestra que hay actividad volcanica, activa todos los sistemas de seguridad y alarma. 
+*/
+
+void alertPeople() { 
+    tone(6, 1000, 500); //Activa el Buzzer en el pin 6 con una frecuencia de 1000 Hz y 500 milisegundos de duración 
+    
+    //Encender LED's por 250 milisegundos
+    digitalWrite(3, 1);
+    digitalWrite(13, 1); 
+    delay(250);
+    //Apagar LED's por 250 milisegundos
+    digitalWrite(3, 0);
+    digitalWrite(13, 0);
+    delay(250);
+    //Encender LED's por 250 milisegundos
+    digitalWrite(3, 1);
+    digitalWrite(13, 1);
+    delay(250);
+    //Apagar LED's por 250 milisegundos
+    digitalWrite(3, 0);
+    digitalWrite(13, 0);
+    delay(250);
+    tone(6, 1000, 500); //Activa el Buzzer en el pin 6 con una frecuencia de 1000 Hz y 500 milisegundos de duración 
+    
+    //Encender LED's por 250 milisegundos
+    digitalWrite(3, 1);
+    digitalWrite(13, 1);
+    delay(250);
+    //Apagar LED's por 250 milisegundos
+    digitalWrite(3, 0);
+    digitalWrite(13, 0);
+    delay(250);
+    //Encender LED's por 250 milisegundos
+    digitalWrite(3, 1);
+    digitalWrite(13, 1);
+    delay(250);
+    //Apagar LED's por 20 milisegundos
+    digitalWrite(3, 0);
+    digitalWrite(13, 0);
+    delay(20);
+}
